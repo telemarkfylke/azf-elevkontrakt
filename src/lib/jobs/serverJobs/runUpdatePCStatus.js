55c5b77d3a14e0b954535a3d672bf9d08c43dbdb
Robin Ellingsen <robin-elli@hotmail.com>
2025-10-22 16:01:18 +0200

    refactor(runUpdatePCStatus): convert IIFE to named async function

--- src/lib/jobs/serverJobs/runUpdatePCStatus.js
+++ src/lib/jobs/serverJobs/runUpdatePCStatus.js
@@ -1,16 +1,19 @@
-(async () => {
-    require('dotenv').config();
-    const { logger } = require('@vtfk/logger');
-    const { updateStudentPCStatus } = require('./updateStudentPCStatus.js');
+const runJob = async () => {
+    require('dotenv').config()
+    const { logger } = require('@vtfk/logger')
+    const { updateStudentPCStatus } = require('./updateStudentPCStatus.js')
 
-    logger('info', ['Starting updateStudentPCStatus job']);
+    logger('info', ['Starting updateStudentPCStatus job'])
 
-    logger('info', ['Updating student PC status for utlevering']);
+    logger('info', ['Updating student PC status for utlevering'])
     const statusUpdateUtlevering = await updateStudentPCStatus('utlevering')
-    logger('info', [`Finished updateStudentPCStatus job for utlevering. Number of utlevert: ${statusUpdateUtlevering.updateCount}`]);
+    logger('info', [`Finished updateStudentPCStatus job for utlevering. Number of utlevert: ${statusUpdateUtlevering.updateCount}`])
 
-    // logger('info', ["Updating student PC status fir innlevering"])
+    // logger('info', ["Updating student PC status for innlevering"])
     // const statusUpdateInnlevering = await updateStudentPCStatus('innlevering')
     // logger('info', [`Finished updateStudentPCStatus job for innlevering. Number of innlevert: ${statusUpdateInnlevering.updateCount}`])
+
     return true
-})()
+}
+
+runJob()
