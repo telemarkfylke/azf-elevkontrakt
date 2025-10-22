71f031a8b035c65bd82eef2e0dd51d3d955a76ae
Robin Ellingsen <robin-elli@hotmail.com>
2025-10-22 12:51:57 +0200

    feat(pc-status): add job to update student PC status and handle CSV file updates

---
+++ src/lib/jobs/serverJobs/runUpdatePCStatus.js
@@ -0,0 +1,13 @@
+(async () => {
+    require('dotenv').config();
+    const { logger } = require('@vtfk/logger');
+    const { updateStudentPCStatus } = require('./updateStudentPCStatus.js');
+    const fs = require('fs').promises;
+
+    logger('info', ['Starting updateStudentPCStatus job']);
+
+    logger('info', ['Updating student PC status for utlevering']);
+
+    // await updateStudentPCStatus('utlevering');
+    logger('info', ['Finished updateStudentPCStatus job for utlevering']);
+})()
