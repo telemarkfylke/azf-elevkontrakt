26f8bb546e124092aa6a46d3f7d27a4474f0ea23
robinElli <robin-elli@hotmail.com>
2026-06-25 09:12:58 +0200

    feat: update sync schedule for Pureservice students to every 15 minutes

--- src/functions/syncPureserviceStudents.js
+++ src/functions/syncPureserviceStudents.js
@@ -2,8 +2,10 @@ const { app } = require('@azure/functions')
 const { syncPureserviceStudents } = require('../lib/jobs/syncPureserviceStudents')
 
 app.timer('syncPureserviceStudentsRegular', {
-  // Once every day at 07:00 AM
-  schedule: '0 7 * * *',
+  // Every 15 minutes between August and December, and January to June
+  // This will trigger a change stream sync for students in Pureservice, ensuring that any updates to student records are reflected in the system minimum 15 minutes after a contract is created.
+  // The pureserviceId is needed to push info from the contract to the student record in Pureservice, so this function ensures that the pureserviceId is set on the student record in a timely manner.
+  schedule: '0 */15 * * 8-12,1-6 *',
   handler: async (myTimer, context) => {
     try {
       const report = await syncPureserviceStudents('regular')
